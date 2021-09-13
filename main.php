37237aaeeae462b9750ebac1363d51be50fbe567
KornJunge <89209272+KornJunge@users.noreply.github.com>
2021-09-13 21:27:19 +0200

    Add files via upload

--- main.php
+++ main.php
@@ -1,6 +1,7 @@
 <?php
     if($embedded) {  
-        echo "<script>var active = 'main01';</script>";
+        echo "<script>var active = 'main01';
+                      var target = 'left_page_content_01';</script>";
     
         $landscape = new Page("landscape",
                      [new Area("landscape_main"),
@@ -16,39 +17,57 @@
         
         
         
-        $videos = [ new Video("main01", "content/homepage_main_loop.mp4", 1), 
-                    new Video("test01", "content/Untitled2.mp4", 1),
+        $landscape_videos = new Videos("landscape_videos",[ 
+                    new Video("main", "content/homepage_main_loop.mp4", 1),
                     new Video("links_nach", "content/homepage_links_nach.mp4", 0),
                     new Video("links", "content/homepage_links.mp4", 1),
                     new Video("links_von", "content/homepage_links_von.mp4", 0),
-                    ];
+                    new Video("rechts_nach", "content/homepage_rechts_nach.mp4", 0),
+                    new Video("rechts", "content/homepage_rechts.mp4", 1),
+                    new Video("rechts_von", "content/homepage_rechts_von.mp4", 0),
+                    ]);
                     
-        $videos[0]->load();
+        $landscape_videos->getVideo("main")->load();
         
         
         
         
         
-        $landscape->addContent(new Content("main_page_content_01", 100, 70, 30, 0, 0, "Ich habe ein Pupsgesicht :-*"));
+        $landscape->getArea(0)->addContent(new Content("main_page_content_01", 100, 70, 30, -5, 0, "Zeitraffer"));
+        $landscape->getArea(0)->addContent(new Content("main_page_content_02", 100, 70, 30, -5, 0, "Fotos"));
         
-        $landscape->addLink(new Link("pause_button", false, 0.2, "content/pause.gif", false, 8, 8, 0, 45, 1));
-        $landscape->addLink(new Link("play_button", false, 0.2, "content/play.gif", false, 8, 8, 0, 45, 0));
+        $landscape->getArea(1)->addContent(new Content("left_page_content_01", 110, 75, 20, -5, 0, "Kontakt"));
+        $landscape->getArea(1)->addContent(new Content("left_page_content_02", 110, 75, 20, -5, 0, "Impressum"));
         
-        $landscape->getArea(0)->addLink(new Link("main_link01", "Video 1", 0.6, false, "matrix(1, 0.01, 0, 1, 0, 0)", 16, 6, -50.5, -24.8, 1));
-        $landscape->getArea(0)->addLink(new Link("test_link01", "Video 2", 0.6, false, "matrix(1, 0.01, 0, 1, 0, 0)", 16, 6, -50.5, -18.5, 1));
-        $landscape->getArea(0)->addLink(new Link("main_content01_on", "Fotos", 0.6, false, "matrix(1, 0.2, 0, 1, 0, 0)", 16, 6, -75, -39.5, 1));
-        $landscape->getArea(0)->addLink(new Link("main_content01_off", "Fotos", 0.6, false, "matrix(1, 0.2, 0, 1, 0, 0)", 16, 6, -75, -39.5, 0));
-        $landscape->getArea(0)->addLink(new Link("left_content", "links!", 0.6, false, "matrix(1, 0.2, 0, 1, 0, 0)", 16, 6, -75, -33, 1));
+        $landscape->getArea(2)->addContent(new Content("right_page_content_01", 120, 75, 10, 0, 0, "Service"));
+        $landscape->getArea(2)->addContent(new Content("right_page_content_02", 120, 75, 10, 0, 0, "&Uuml;ber mich"));
+        // "Ich habe ein Pupsgesicht :-*"
+        $landscape->addLink(new Link(0,"pause_button", false, 0.2, "content/pause.gif", false, 8, 8, 0, 45, 1));
+        $landscape->addLink(new Link(0,"play_button", false, 0.2, "content/play.gif", false, 8, 8, 0, 45, 0));
         
-        $landscape->getArea(1)->addLink(new Link("left_link01", "zur&uuml;ck", 0.6, false, "matrix(1, 0, 0, 1, 0, 0)", 16, 6, 40, -30.8, 0));
+        $landscape->getArea(0)->addLink(new Link(0, "right_content01", "Service", 0.6, false, "matrix(1, 0, 0, 1, 0, 0)", 16, 6, -53, -24.4, 1));
+        $landscape->getArea(0)->addLink(new Link(0, "right_content02", "&Uuml;ber mich", 0.6, false, "matrix(1, 0, 0, 1, 0, 0)", 22, 6, -53, -18.1, 1));
+        $landscape->getArea(0)->addLink(new Link(0, "main_content01_on", "Zeitraffer", 0.6, false, "matrix(1, 0.18, 0, 1, 0, 0)", 16, 6, -76, -39.5, 1));
+        $landscape->getArea(0)->addLink(new Link(0, "main_content01_off", "Zeitraffer", 0.6, false, "matrix(1, 0.18, 0, 1, 0, 0)", 16, 6, -76, -39.5, 0));
+        $landscape->getArea(0)->addLink(new Link(0, "main_content02_on", "Fotos", 0.6, false, "matrix(1, 0.18, 0, 1, 0, 0)", 16, 6, -76, -33, 1));
+        $landscape->getArea(0)->addLink(new Link(0, "main_content02_off", "Fotos", 0.6, false, "matrix(1, 0.18, 0, 1, 0, 0)", 16, 6, -76, -33, 0));
+        $landscape->getArea(0)->addLink(new Link(0, "left_content01", "Kontakt", 0.6, false, "matrix(1, 0.01, 0, 1, 0, 0)", 16, 6, -79, -25, 1));
+        $landscape->getArea(0)->addLink(new Link(0, "left_content02", "Impressum", 0.6, false, "matrix(1, 0.01, 0, 1, 0, 0)", 16, 6, -79, -18.5, 1));
+        
+        $landscape->getArea(1)->addLink(new Link(1, "left_link01", false, 0.2, "content/zurueck.gif", false, 8, 8, 80, 45, 1));
+        
+        $landscape->getArea(2)->addLink(new Link(2, "right_link01", false, 0.2, "content/zurueck_inverted.gif", false, 8, 8, -80, 45, 1));
         
         
         
         
-        $videos[2]->onEnd($landscape, "links", 1);
-        $videos[4]->onEnd($landscape, "main01", 0);
+        $landscape_videos->getVideo("links_nach")->onEnd($landscape, "links", 1);
+        $landscape_videos->getVideo("links_von")->onEnd($landscape, "main", 0);
+        $landscape_videos->getVideo("rechts_nach")->onEnd($landscape, "rechts", 2);
+        $landscape_videos->getVideo("rechts_von")->onEnd($landscape, "main", 0);
         
-        
+        $landscape_videos->getVideo("links")->onStart($landscape, ["displayTarget"]);
+        $landscape_videos->getVideo("rechts")->onStart($landscape, ["displayTarget"]);
         
         
         $landscape->getLink("pause_button")->addActions(new Actions([
@@ -63,22 +82,13 @@
                             new Action("play_button","style.display = 'none'"),
                             ])); 
                             
-       $landscape->getArea(0)->getLink("main_link01")->addActions(new Actions([
-                            $videos[0]->disable(),
-                            $videos[1]->enable(),
-                            new Action("pause_button","style.display = ''"),
-                            new Action("play_button","style.display = 'none'"),
-                            ]));
-        
-        $landscape->getArea(0)->getLink("test_link01")->addActions(new Actions([
-                            $videos[1]->disable(),
-                            $videos[0]->enable(),
-                            new Action("pause_button","style.display = ''"),
-                            new Action("play_button","style.display = 'none'"),
-                            ]));
+
                             
         $landscape->getArea(0)->getLink("main_content01_on")->addActions(new Actions([
                             $landscape->loadContent("main_page_content_01"),
+                            $landscape->unloadContent("main_page_content_02"),
+                            new Action("main_content02_off","style.display = 'none'"),
+                            new Action("main_content02_on","style.display = ''"), 
                             new Action("main_content01_on","style.display = 'none'"),
                             new Action("main_content01_off","style.display = ''"),  
                             ]));
@@ -88,18 +98,61 @@
                             new Action("main_content01_off","style.display = 'none'"),
                             new Action("main_content01_on","style.display = ''"),  
                             ]));
+                            
+        $landscape->getArea(0)->getLink("main_content02_on")->addActions(new Actions([
+                            $landscape->unloadContent("main_page_content_01"),
+                            $landscape->loadContent("main_page_content_02"),
+                            new Action("main_content01_off","style.display = 'none'"),
+                            new Action("main_content01_on","style.display = ''"), 
+                            new Action("main_content02_on","style.display = 'none'"),
+                            new Action("main_content02_off","style.display = ''"),  
+                            ]));
+                            
+        $landscape->getArea(0)->getLink("main_content02_off")->addActions(new Actions([
+                            $landscape->unloadContent("main_page_content_02"),
+                            new Action("main_content02_off","style.display = 'none'"),
+                            new Action("main_content02_on","style.display = ''"),  
+                            ]));
         
-        $landscape->getArea(0)->getLink("left_content")->addActions(new Actions([
+        $landscape->getArea(0)->getLink("left_content01")->addActions(new Actions([
                             $landscape->unloadArea(0),
-                            $videos[0]->disable(),
-                            $videos[2]->enable(),  
+                            new Action("left_page_content_01","setTarget"),
+                            $landscape_videos->getVideo("main")->disable(),
+                            $landscape_videos->getVideo("links_nach")->enable(),  
+                            ]));
+                            
+        $landscape->getArea(0)->getLink("left_content02")->addActions(new Actions([
+                            $landscape->unloadArea(0),
+                            new Action("left_page_content_02","setTarget"),
+                            $landscape_videos->getVideo("main")->disable(),
+                            $landscape_videos->getVideo("links_nach")->enable(),  
+                            ]));
+                            
+        $landscape->getArea(0)->getLink("right_content01")->addActions(new Actions([
+                            $landscape->unloadArea(0),
+                            new Action("right_page_content_01","setTarget"),
+                            $landscape_videos->getVideo("main")->disable(),
+                            $landscape_videos->getVideo("rechts_nach")->enable(),  
+                            ]));
+                            
+        $landscape->getArea(0)->getLink("right_content02")->addActions(new Actions([
+                            $landscape->unloadArea(0),
+                            new Action("right_page_content_02","setTarget"),
+                            $landscape_videos->getVideo("main")->disable(),
+                            $landscape_videos->getVideo("rechts_nach")->enable(),  
                             ]));
                             
         $landscape->getArea(1)->getLink("left_link01")->addActions(new Actions([
                             $landscape->unloadArea(1),
-                            $videos[3]->disable(),
-                            $videos[4]->enable(),  
-                            ]));         
+                            $landscape_videos->getVideo("links")->disable(),
+                            $landscape_videos->getVideo("links_von")->enable(),
+                            ]));
+                            
+        $landscape->getArea(2)->getLink("right_link01")->addActions(new Actions([
+                            $landscape->unloadArea(2),
+                            $landscape_videos->getVideo("rechts")->disable(),
+                            $landscape_videos->getVideo("rechts_von")->enable(), 
+                            ]));       
                                                   
         echo '</div>';
     }
--- scripts.php
+++ scripts.php
@@ -1,40 +1,118 @@
 <?php
-    if($embedded) {  
-        class Video {
-            private $name;
-            private $href;
+    if($embedded) { 
+    
+        class Videos {
+            private $videos = array();
+            public function __construct($input_id, $input_videos) {
+                $this->videos = $input_videos;    
+            }
+            public function getVideo($input_id) {
+                return $this->videos[array_search($input_id, $this->getIDs())];
+            }
             
-            public function __construct($input_id, $input_href) {
+            public function getIDs() {
+                $ids = array();
+                for($i = 0; $i < count($this->videos); $i++) {
+                    $ids[] = $this->videos[$i]->getID();
+                }
+                return $ids;
+            }
+        } 
+        
+        
+        class Video {
+            private $id;
+            private $href;
+            private $active = false;
+            
+            public function __construct($input_id, $input_href, $input_loop) {
                 $this->id = $input_id;
                 $this->href = $input_href;
-                echo '<video id="'.$this->id.'" autoplay loop muted>';
+                
+                // Create Video
+                echo '<video id="'.$this->id.'" preload="auto" muted';
+                if($input_loop == 1) {
+                    echo ' loop ';
+                }
+                echo '>';
                 echo 'Your browser does not support the video tag.';
                 echo '<source src="'.$this->href.'" type="video/mp4">'; 
                 echo '</video>';
                 echo '<script>document.getElementById("'.$this->id.'").style.display="none"</script>';  
             }
+            
+            public function onEnd($input_parent, $input_ended, $input_area) {
+                // Ended functionality
+                if($input_ended != "") {
+                    echo '<script type="text/javascript">';
+                    echo 'document.getElementById("'.$this->id.'").addEventListener("ended",onEnd'.$this->id.',false);';
+                    echo 'function onEnd'.$this->id.'(e) {';
+                    (new Actions([$this->disable()]))->getActions();
+                    echo "active = ".json_encode($input_ended).";";
+                    (new Actions([$input_parent->loadArea($input_area)]))->getActions();
+                    echo 'document.getElementById("'.$input_ended.'").style.display="";';
+                    echo 'document.getElementById("'.$input_ended.'").play()';
+                    echo '}';
+                    echo '</script>';
+                }
+            }
+            
+             public function onStart($input_parent, $input_start) {
+                // Started functionality
+                if($input_start != "") {
+                    echo '<script type="text/javascript">';
+                    echo 'document.getElementById("'.$this->id.'").addEventListener("play",onStart'.$this->id.',false);';
+                    echo 'function onStart'.$this->id.'(e) {';
+                    if ($input_start[0] == "displayTarget") {
+                       echo 'document.getElementById(target).style.display="";'; 
+                       
+                    } else {
+                        echo 'document.getElementById("'.$input_start[0].'").style.display="";';
+                    }
+                    echo '}';
+                    echo '</script>';
+                }
+            }
+            
+            public function getID() {
+                return $this->id;
+            }
+           
+        
+            public function load() {
+                echo '<script>document.getElementById("'.$this->id.'").play()</script>'; 
+                echo '<script>document.getElementById("'.$this->id.'").style.display=""</script>';
+                echo "<script>active = ".json_encode($this->id).";</script>";   
+            }
         
             public function enable() {
-                echo '<script>document.getElementById("'.$this->id.'").style.display=""</script>';   
+                return new Action([$this->id,$this->id,$this->id,"pause_button","play_button"],["setActive","play()", "style.display = ''","style.display = ''","style.display = 'none'"]);
             }
             
             public function disable() {
-                echo '<script>document.getElementById("'.$this->id.'").style.display="none"</script>';   
+                return new Action([$this->id,$this->id,$this->id],["pause()","currentTime = 0" , "style.display = 'none'"]);
             }
+            
         }
         
         
         
-        class Link {
-        
-            public function __construct($input_id, $input_width, $input_height, $input_x, $input_y, $input_actions) {
+        class Content {
+            public $id;
+            public $content; 
+            public function __construct($input_id, $input_width, $input_height, $input_x, $input_y, $input_visible, $input_content) {
+                $this->id = $input_id;
+                $this->content = $input_content
                 ?>
                 <style> 
                     #<?php echo $input_id; ?>{
-                        background:blue;
+                        background-color: #000D;
+                        border-radius: 0.2em;
                         position:absolute;
-                        cursor:pointer;
                         transform: translate(-50%, -50%);
+                        font-size: 50px;
+                        color: white;
+                        padding: 10px;
                     }
               
                     @media screen and (max-aspect-ratio: 1920/1080) {
@@ -55,10 +133,83 @@
                         }
                     }
                 </style>
-                <div id="<?php echo $input_id; ?>" onclick="<?php echo $input_id; ?>Execute()"></div>
                 
+                <div id="<?php echo $input_id; ?>"  
+                            <?php if(!$input_visible) { ?>
+                            style="display: none;"
+                        <?php 
+                        } 
+                        echo ">".$input_content."</div>";
+
+            }
+            
+            public function getID() {
+                return $this->id;
+            }
+        }
+        
+        
+        
+        class Link {
+            public $id;
+            public $defaultVisible;
+            public $area;
+            public function __construct($input_area, $input_id, $input_text, $input_opacity, $input_image, $input_matrix, $input_width, $input_height, $input_x, $input_y, $input_visible) {
+                $this->id = $input_id;
+                $this->defaultVisible = $input_visible;
+                $this->area = $input_area;
+                ?>
+                <style> 
+                    #<?php echo $input_id; ?>{
+                        background-image: url("<?php echo $input_image; ?>");
+                        background-size: cover;
+                        opacity: <?php echo $input_opacity; ?>;
+                        position:absolute;
+                        cursor:pointer;
+                        transform: translate(-50%, -50%);
+                        color: white;
+                        transform:<?php echo $input_matrix; ?>;
+                        
+                    }
+              
+                    @media screen and (max-aspect-ratio: 1920/1080) {
+                        #<?php echo $input_id; ?>{
+                            top:calc(50% + calc(<?php echo $input_y; ?>vw * 0.5625));
+                            left:calc(50% + calc(<?php echo $input_x; ?>vw * 0.5625));
+                            height:calc(<?php echo $input_height; ?>vw * 0.5625);
+                            width:calc(<?php echo $input_width; ?>vw * 0.5625);
+                            font-size: calc(5vw * 0.5625);
+                        }
+                    }
+                    
+                    @media screen and (min-aspect-ratio: 1920/1080) {
+                        #<?php echo $input_id; ?>{
+                            top:calc(50% + <?php echo $input_y; ?>vh);
+                            left:calc(50% + <?php echo $input_x; ?>vh);
+                            height:<?php echo $input_height; ?>vh;
+                            width:<?php echo $input_width; ?>vh;
+                            font-size: 5vh;
+                        }
+                    }
+                </style>
+                <?php
+                echo '<div id="'.$input_id.'" onclick="'.$input_id.'Execute()"';  
+                if(!$input_visible || $this->area != 0) {
+                    echo 'style="display: none;"';
+                }
+                echo '>';
+                echo $input_text;
+                echo '</div>';  
+            }
+            
+            public function getVisible() {
+                return $this->defaultVisible;
+            }
+            
+            public function addActions($input_actions) {
+                ?>
                 <script>
-                function <?php echo $input_id; ?>Execute(){
+                function <?php echo $this->id; ?>Execute(){
 
                     <?php
                     echo $input_actions->getActions();
@@ -66,9 +217,12 @@
   
                 }
                 </script>
-
             
              <?php
+            }
+            
+            public function getID() {
+                return $this->id;
             }
         }
         
@@ -88,7 +242,29 @@
         class Action {
             private $action;
             public function __construct($input_target, $input_command) {
-                $this->action = "document.getElementById('".$input_target."').".$input_command.";";    
+                if(is_array($input_target)) {
+                    for($i = 0; $i < count($input_target); $i++) {
+                        if($input_target[$i] == "activeVideo") {
+                            $this->action .= "document.getElementById(active).".$input_command[$i].";\n";
+                        } elseif ($input_command[$i] == "setActive") {
+                            $this->action .= "active = ".json_encode($input_target[$i]).";\n";
+                        } elseif ($input_command[$i] == "setTarget") {
+                            $this->action .= "target = ".json_encode($input_target[$i]).";\n"; 
+                        } else {
+                            $this->action .= "document.getElementById('".$input_target[$i]."').".$input_command[$i].";\n";
+                        }
+                    }
+                } else {
+                    if($input_target == "activeVideo") {
+                        $this->action .= "document.getElementById(active).".$input_command.";";
+                    } elseif ($input_command == "setActive") {
+                            $this->action = "active = ".json_encode($input_target).";"; 
+                    } elseif ($input_command == "setTarget") {
+                            $this->action = "target = ".json_encode($input_target).";";
+                    } else {
+                        $this->action = "document.getElementById('".$input_target."').".$input_command.";";
+                    } 
+                }   
             }
             
             public function print() {
@@ -101,49 +277,103 @@
         class Page {
 
             public $id;
+            public $areas = array();
             public $links = array();
             public $content = array();
             
-            public function __construct($input_name) {
-                $this->name = $input_name;
+            public function __construct($input_id, $input_areas) {
+                $this->id = $input_id;
+                $this->areas = $input_areas;
             }
             
             public function addLink($input_link) {
                 $this->links[] = $input_link;
-                
+            }
+            
+            public function getLink($input_id) {
+                return $this->links[array_search($input_id, $this->getIDs())];
+            }
+            
+            public function getIDs() {
+                $ids = array();
+                for($i = 0; $i < count($this->links); $i++) {
+                    $ids[] = $this->links[$i]->getID();
+                }
+                return $ids;
             }
             
             public function addContent($input_content) {
                 $this->content[] = $input_content;
             }
             
-          
+            public function unloadArea($input_id) { 
+                $ids = array();
+                $commands = array();
+                //unload Links
+                for($i = 0; $i < count($this->areas[$input_id]->links); $i++) {
+                   $ids[] = $this->areas[$input_id]->links[$i]->getID();
+                   $commands[] = "style.display = 'none'";
+                }
+                //unload Content
+                for($i = 0; $i < count($this->areas[$input_id]->content); $i++) {
+                   $ids[] = $this->areas[$input_id]->content[$i]->getID();
+                   $commands[] = "style.display = 'none'";
+                }
+                return new Action($ids, $commands);
+            }
             
-        
+            public function loadArea($input_id) { 
+                $ids = array();
+                $commands = array();
+                //load Links
+                for($i = 0; $i < count($this->areas[$input_id]->links); $i++) {
+                    if($this->areas[$input_id]->links[$i]->getVisible()) {
+                        $ids[] = $this->areas[$input_id]->links[$i]->getID();
+                        $commands[] = "style.display = ''";
+                    }
+                }
+                return new Action($ids, $commands);
+            }
+
+            public function loadContent($input_content) {
+                $ids = array();
+                $commands = array();
+                for($i = 0; $i < count($this->content); $i++) {
+                   $ids[] = $this->content[$i]->getID();
+                   $commands[] = "style.display = 'none'";   
+                }
+                $ids[] = $input_content;
+                $commands[] = "style.display = ''";
+                return new Action($ids, $commands);
+            }
+            
+            public function unloadContent($input_content) {
+                $ids = array();
+                $commands = array();
+                for($i = 0; $i < count($this->content); $i++) {
+                   $ids[] = $this->content[$i]->getID();
+                   $commands[] = "style.display = 'none'";   
+                }
+                $ids[] = $input_content;
+                $commands[] = "style.display = 'none'";
+                return new Action($ids, $commands);
+            }
+            
+            public function getArea($input_id) {
+                return $this->areas[$input_id];
+            }
         } 
         
         
+        class Area extends Page {
         
-        ?>
-        <script>
-          //video = document.getElementById('main01');
-          //document.getElementById('video_main').style.display = 'none';
-          //source = document.getElementById('source');
-          //<video onclick="this.paused? this.play() : this.pause()">
-          
-          
-          //function PlayVideo(){
-         // document.getElementById('main01').pause();
-          //document.getElementById("main01").style.display="none";
-         // document.getElementById("test01").style.display="";
-            //video.pause();
-            //video.style.display = 'none';
-            //video.load();
-            //video.play();
-         // }
-          
-        </script>
+            public $id;
+            
+            public function __construct($input_id) {
+                $this->id = $input_id;
+            }
+        }
+        
 
-    <?php
     }
 ?>
